c45ee8c7e50e22b2f3dfd22ae60670890c622105
tino-hentschel-haw <94183473+tino-hentschel-haw@users.noreply.github.com>
2022-01-16 12:10:21 +0100

    add OrientedPoint struct to move 2D shapes along the 3D curve

---
+++ Assets/Scripts/OrientedPoint.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public struct OrientedPoint
+{
+    public Vector3 Position;
+    public Quaternion Rotation;
+
+    public OrientedPoint(Vector3 position, Quaternion rotation)
+    {
+        Position = position;
+        Rotation = rotation;
+    }
+
+    public OrientedPoint(Vector3 position, Vector3 forward)
+    {
+        Position = position;
+        Rotation = Quaternion.LookRotation(forward);
+    }
+
+    public Vector3 LocalToWorld(Vector3 localSpacePoint)
+    {
+        // Rotation * localSpacePoint => Local To World conversation for a rotation.
+        return Position + Rotation * localSpacePoint;
+    }
+}
--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEditor;
 
@@ -13,21 +12,35 @@ public class Segment : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        for (int i = 0; i < 4; i++)
+        for (var i = 0; i < 4; i++)
         {
             Gizmos.DrawSphere(GetPos(i), 0.05f);
         }
 
         Handles.DrawBezier(GetPos(0), GetPos(3), GetPos(1), GetPos(2), Color.white, EditorGUIUtility.whiteTexture, 1f);
 
-        var testPoint = GetBezierPoint(tTest);
+        
+        var testPoint = GetBezierOrientedPoint(tTest);
+        var radius = 0.01f;
+        void DrawPoint(Vector3 localPos) => Gizmos.DrawSphere(testPoint.LocalToWorld(localPos), radius);
+        
+
+        Handles.PositionHandle(testPoint.Position, testPoint.Rotation);
         
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(testPoint, 0.05f);
+        
+        DrawPoint(Vector3.right * 0.1f);
+        DrawPoint(Vector3.right * 0.2f);
+        DrawPoint(Vector3.right * -0.1f);
+        DrawPoint(Vector3.right * -0.2f);
+        
+        DrawPoint(Vector3.up * 0.1f);
+        DrawPoint(Vector3.up * 0.2f);
+
         Gizmos.color = Color.white;
     }
 
-    private Vector3 GetBezierPoint(float t)
+    private OrientedPoint GetBezierOrientedPoint(float t)
     {
         var p0 = GetPos(0);
         var p1 = GetPos(1);
@@ -41,6 +54,9 @@ public class Segment : MonoBehaviour
         var d = Vector3.Lerp(a, b, t);
         var e = Vector3.Lerp(b, c, t);
 
-        return Vector3.Lerp(d, e, t);
+        var pos = Vector3.Lerp(d, e, t);
+        var tangent = (e-d).normalized;
+
+        return new OrientedPoint(pos, tangent);
     }
 }
